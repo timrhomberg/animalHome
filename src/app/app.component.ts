ac67de48d4880ac399dc324b5cddd4bacde422a3
rhombtim <rhombtim@students.zhaw.ch>
2020-09-23 21:35:44 +0200

    Add new content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'animalHome';
+  constructor(private router: Router) {
+  }
+
+  login(): void {
+    this.router.navigate(['login']);
+  }
+
+  goDashboard(): void {
+    this.router.navigate(['dashboard']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    DashboardComponent
+  ]
 })
 export class AppModule { }
